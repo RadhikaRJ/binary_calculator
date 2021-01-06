95217e99ca9e5ede22650568a9cae3c25874caac
Radhika Joshi <radhika7002@gmail.com>
2021-01-06 16:33:26 +0530

    feat: calculation

--- binaryCalculator.js
+++ binaryCalculator.js
@@ -1,4 +1,6 @@
-  let btns = document.querySelectorAll("#btnSum, #btnSub, #btnMul, #btnDiv, #btn0, #btn1, #btnClr");
+"use strict";
+
+let btns = document.querySelectorAll("#btnSum, #btnSub, #btnMul, #btnDiv, #btn0, #btn1, #btnClr");
 
 for(let i=0;i<btns.length;i++){
     btns[i].onclick = function(){
@@ -9,3 +11,8 @@ for(let i=0;i<btns.length;i++){
         }
         }
     }
+document.getElementById("btnEql").onclick = function(){
+    let arr = document.getElementById("res").innerHTML.match(/([01]+)([\+\-\*\/])([01]+)/);
+    let n = parseInt(arr[1],2)+arr[2]+parseInt(arr[3],2);
+    document.getElementById("res").innerHTML = Math.floor(eval(n)).toString(2);
+}
